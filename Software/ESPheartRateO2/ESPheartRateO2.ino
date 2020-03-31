c6647e1ab27e5ac9ae02196ae4a000e5244b9c5d
unknown <mwest.lazerade@gmail.com>
2020-03-31 16:11:45 -0600

    adding software folder for Arduino sketches and sensor data logs

---
+++ Software/ESPheartRateO2/ESPheartRateO2.ino
@@ -0,0 +1,188 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <Client.h>
+#include <heartRate.h>
+#include <MAX30105.h>
+#include <spo2_algorithm.h>
+#include "spo2_algorithm.h"
+
+
+#include <CayenneArduinoDefines.h>
+#include <CayenneArduinoMQTTClient.h>
+#include <CayenneHandlers.h>
+#include <CayenneMessage.h>
+#include <CayenneMQTTESP8266.h>
+#include <CayenneMQTTWiFiClient.h>
+#include <DetectDevice.h>
+
+// This example shows how to connect to Cayenne using an ESP8266 and send/receive sample data.
+// Make sure you install the ESP8266 Board Package via the Arduino IDE Board Manager and select the correct ESP8266 board before compiling.
+
+const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
+byte rates[RATE_SIZE]; //Array of heart rates
+byte rateSpot = 0;
+long lastBeat = 0; //Time at which the last beat occurred
+
+float beatsPerMinute;
+int beatAvg;
+
+//#define CAYENNE_DEBUG
+#define CAYENNE_PRINT Serial
+#define MAX_BRIGHTNESS 255
+
+#if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__)
+//Arduino Uno doesn't have enough SRAM to store 100 samples of IR led data and red led data in 32-bit format
+//To solve this problem, 16-bit MSB of the sampled data will be truncated. Samples become 16-bit data.
+uint16_t irBuffer[100]; //infrared LED sensor data
+uint16_t redBuffer[100];  //red LED sensor data
+#else
+uint32_t irBuffer[100]; //infrared LED sensor data
+uint32_t redBuffer[100];  //red LED sensor data
+#endif
+
+int32_t bufferLength; //data length
+int32_t spo2; //SPO2 value
+int8_t validSPO2; //indicator to show if the SPO2 calculation is valid
+int32_t heartRate; //heart rate value
+int8_t validHeartRate; //indicator to show if the heart rate calculation is valid
+
+
+
+
+MAX30105 HRO2;//MAX heart rate sensor
+
+
+
+// WiFi network info.
+char ssid[] = "BearsAndBears"; //Place your SSID within the quotations
+char wifiPassword[] = "dudeson666"; //Place your wifi password within the quotations
+
+// Cayenne authentication info. This should be obtained from the Cayenne Dashboard.
+char username[] = "59d72100-1298-11e8-9f38-9fae3d42ebf0"; //Place username from your account within the quotations
+char password[] = "9314ad7b109d3cbb67591777d2f61dde2b549663"; //Place password from your account within the quotations.
+char clientID[] = "16a2aa40-72f3-11ea-84bb-8f71124cfdfb"; //Place Client ID from your account within the quotations.
+
+unsigned long lastMillis = 0;
+int O2 = 0;
+int HR = 0;
+
+
+
+
+
+
+
+void setup() {
+  Serial.begin(9600);
+  Cayenne.begin(username, password, clientID, ssid, wifiPassword);
+
+  // Initialize sensor
+  if (!HRO2.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+  {
+    Serial.println("MAX30105 was not found. Please check wiring/power. ");
+    while (1);
+  }
+  Serial.println("Place your index finger on the sensor with steady pressure.");
+
+  byte ledBrightness = 60; //Options: 0=Off to 255=50mA
+  byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
+  byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+  byte sampleRate = 100; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int pulseWidth = 411; //Options: 69, 118, 215, 411
+  int adcRange = 4096; //Options: 2048, 4096, 8192, 16384
+
+  HRO2.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
+
+}
+
+void loop() {
+  Cayenne.loop();
+  bufferLength = 100; //buffer length of 100 stores 4 seconds of samples running at 25sps
+
+  //read the first 100 samples, and determine the signal range
+  for (byte i = 0 ; i < bufferLength ; i++)
+  {
+    while (HRO2.available() == false) //do we have new data?
+      HRO2.check(); //Check the sensor for new data
+
+    redBuffer[i] = HRO2.getRed();
+    irBuffer[i] = HRO2.getIR();
+    HRO2.nextSample(); //We're finished with this sample so move to next sample
+
+    Serial.print(F("red="));
+    Serial.print(redBuffer[i], DEC);
+    Serial.print(F(", ir="));
+    Serial.println(irBuffer[i], DEC);
+  }
+
+  //calculate heart rate and SpO2 after first 100 samples (first 4 seconds of samples)
+  maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+
+  //Continuously taking samples from MAX30102.  Heart rate and SpO2 are calculated every 1 second
+  while (1)
+  {
+    //dumping the first 25 sets of samples in the memory and shift the last 75 sets of samples to the top
+    for (byte i = 25; i < 100; i++)
+    {
+      redBuffer[i - 25] = redBuffer[i];
+      irBuffer[i - 25] = irBuffer[i];
+    }
+
+    //take 25 sets of samples before calculating the heart rate.
+    for (byte i = 75; i < 100; i++)
+    {
+      while (HRO2.available() == false) //do we have new data?
+        HRO2.check(); //Check the sensor for new data
+
+      //digitalWrite(readLED, !digitalRead(readLED)); //Blink onboard LED with every data read
+
+      redBuffer[i] = HRO2.getRed();
+      irBuffer[i] = HRO2.getIR();
+      HRO2.nextSample(); //We're finished with this sample so move to next sample
+
+      //send samples and calculation result to terminal program through UART
+      Serial.print(F("red="));
+      Serial.print(redBuffer[i], DEC);
+      Serial.print(F(", ir="));
+      Serial.print(irBuffer[i], DEC);
+
+      Serial.print(F(", HR="));
+      Serial.print(heartRate, DEC);
+
+      Serial.print(F(", HRvalid="));
+      Serial.print(validHeartRate, DEC);
+
+      Serial.print(F(", SPO2="));
+      Serial.print(spo2, DEC);
+
+      Serial.print(F(", SPO2Valid="));
+      Serial.println(validSPO2, DEC);
+      O2 = (int)validSPO2;
+      HR = (int)validHeartRate;
+    }
+
+    //After gathering 25 new samples recalculate HR and SP02
+    maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+  }
+
+
+  //Publish data every  1 minutes (60000 milliseconds). Change this value to publish at a different interval.
+  if (millis() - lastMillis > 60000) {
+    lastMillis = millis();
+    //Write data to Cayenne here. This example just sends the current uptime in milliseconds.
+    Cayenne.virtualWrite(0, lastMillis);
+    Cayenne.virtualWrite(1, HR);
+    Cayenne.virtualWrite(2, O2);
+    
+  }
+}
+
+//With this code you should see "millis" posted to your account.
+
+//Default function for processing actuator commands from the Cayenne Dashboard.
+//You can also use functions for specific channels, e.g CAYENNE_IN(1) for channel 1 commands.
+CAYENNE_IN_DEFAULT()
+{
+  CAYENNE_LOG("CAYENNE_IN_DEFAULT(%u) - %s, %s", request.channel, getValue.getId(), getValue.asString());
+  //Process message here. If there is an error set an error message using getValue.setError(), e.g getValue.setError("Error message");
+}
