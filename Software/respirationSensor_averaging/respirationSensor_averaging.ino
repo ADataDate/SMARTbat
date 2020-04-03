a8cc054bd4a8020498b4df32c63229ff87b52831
unknown <mwest.lazerade@gmail.com>
2020-04-02 18:45:49 -0600

    adding Respiration sensor sketch

---
+++ Software/respirationSensor_averaging/respirationSensor_averaging.ino
@@ -0,0 +1,25 @@
+
+#define respSensorPin A1 
+ 
+void setup(void) {
+  Serial.begin(9600);
+  pinMode(0, INPUT_PULLUP);
+}
+ 
+void loop(void) {
+  float respirationReading;
+  float averageRespiration = 0;
+  float numMeas = 25;
+
+  for(int i = 0; i<numMeas; i++)
+    {
+      respirationReading = analogRead(respSensorPin);
+      averageRespiration = averageRespiration + respirationReading;
+      delay(50);
+    }
+    averageRespiration = averageRespiration/numMeas;
+
+  Serial.print("Respiration: "); 
+  Serial.println(averageRespiration);    
+  delay(25);
+}
